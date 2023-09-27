4de0167a12382efe71e4630123313fe53679ad68
Andreza <dedezasilva@live.com>
2023-09-26 23:19:37 -0300

    feat: aprendendo sobre alter table

---
+++ alter_table.sql
@@ -0,0 +1,42 @@
+create table if not exists produto(
+	codigo_produto integer not null auto_increment, 
+	descricao_produto varchar(30) not null, 
+	preco_produto float not null,
+	
+	primary key(codigo_produto)
+);
+
+-- alterar o campo descricao_produto da tabela produto 
+-- para o tipo varchar(50)
+ALTER TABLE produto
+-- foi utilizado o CHANGE aqui e não estava correto
+MODIFY COLUMN descricao_produto varchar(50);
+
+
+
+select * from produto; 
+
+create table if not exists nota_fiscal(
+	numero_nf integer not null auto_increment,
+	data_nf date not null,
+	valor_nf float not null,
+	
+	primary key(numero_nf)
+);
+
+-- alterar a tabela nota_fiscal, adicionando o campo ICMS, do tipo float depois do campo numero_nf
+ALTER TABLE nota_fiscal 
+ADD COLUMN icms float not null
+AFTER numero_nf;
+
+select * from nota_fiscal;
+
+
+create table if not exists itens(
+	produto_codigo_produto integer not null auto_increment, 
+	nota_fiscal_numero_nf integer not null, 
+	num_item integer not null, 
+	qtde_item integer not null,
+	
+	primary key(produto_codigo_produto)
+);
