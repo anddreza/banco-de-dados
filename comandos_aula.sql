1ce6534bb0634bee6794391dcf4bc3685d22a3f2
Andreza <dedezasilva@live.com>
2023-10-11 16:09:42 -0300

    feat: resolução de alguns exercicios feitos

--- comandos_aula.sql
+++ comandos_aula.sql
@@ -79,4 +79,74 @@ ON clientes.planos_id = planos.id
 WHERE musicas.generos_id = 1
 AND (planos.descricao = 'Full' OR planos.descricao = 'Light');
 
+7) Qual nome do plano que possui mais clientes que colocaram música
+do Gênero "Funk" e que são das gravadoras "Som Livre", "Sony Music"
+ou "USA Discos" e que tem duração menor que 4 minutos. 
+
+SELECT planos.descricao, clientes.login, generos.descricao, 
+gravadoras.nome  
+FROM musicas 
+INNER JOIN musicas_has_clientes 
+ON musicas_has_clientes.musicas_id = musicas.id
+INNER JOIN clientes 
+ON musicas_has_clientes.clientes_id  = clientes.id
+INNER JOIN planos 
+ON planos.id = clientes.planos_id  
+INNER JOIN musicas_has_artistas 
+ON musicas_has_artistas.musicas_id = musicas.id
+INNER JOIN generos  
+ON generos.id = musicas.generos_id  
+INNER JOIN artistas 
+ON musicas_has_artistas.artistas_id = artistas.id
+INNER JOIN gravadoras 
+ON gravadoras.id = artistas.gravadoras_id
+WHERE generos.descricao = 'Funk'
+AND musicas.duracao  < '04:00:00'
+AND gravadoras.nome = 'Som Livre' 
+OR gravadoras.nome = 'Sony Music'
+OR gravadoras.nome = 'USA Discos'
+
+8) Qual o tempo total das músicas que não são do gênero "Gaúcha" e "Pop",
+e que o tempo individual de cada música não ultrapassa 4 minutos
+
+SELECT generos.descricao, musicas.duracao 
+FROM musicas   
+INNER JOIN generos  
+ON generos.id = musicas.generos_id
+WHERE generos.descricao != 'Gaúcha' 
+AND generos.descricao != 'Pop' 
+AND musicas.duracao  < '04:00:00'
+
+9) Qual o nome das músicas do gênero "Rock", 
+da gravadora "Sony Music" e que tem duração
+maior que 10 minutos 
+
+SELECT musicas.nome, generos.descricao, gravadoras.nome, musicas.duracao 
+FROM generos 
+INNER JOIN musicas 
+ON musicas.generos_id  = generos.id 
+INNER JOIN musicas_has_artistas 
+ON musicas_has_artistas.musicas_id = musicas.id
+INNER JOIN artistas 
+ON musicas_has_artistas.artistas_id = artistas.id
+INNER JOIN gravadoras 
+ON gravadoras.id = artistas.gravadoras_id
+WHERE generos.descricao = 'Rock'
+AND gravadoras.nome = 'Sony Music'
+AND musicas.duracao > '10:00:00'
+
+10) Qual o nome das músicas, o nome dos artistas, o nome do gênero e o 
+nome clientes que locaram músicas que foram gravadoras entre 01 de 
+Janeiro de 1993 e 31 de dezembro de 1999?
+
+
+SELECT musicas.nome, artistas.nome, generos.descricao, clientes.login,
+gravadoras.created 
+
+
+11) Qual o nome do gênero que teve mais músicas locadas no período de 01
+de Janeiro de 2015 e 31 de Dezembro de 2020
+
+12) Qual o nome da gravadora, que possui o maior valor de contrato e 
+que não tem nenhuma música "Funk" lançada no ano de 2000?
 
